61eb850008921e3ac6872085557d1468549a0b78
IgnasJJK <ignas.kaveckas@gmail.com>
2015-08-01 19:53:44 +0300

    Enhanced highscore sorting

--- src/me/MnMaxon/LonksKits/Points.java
+++ src/me/MnMaxon/LonksKits/Points.java
@@ -12,35 +12,50 @@ import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.block.Sign;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 
 public class Points {
 	public static ArrayList<String> lastScores = null;
-
+	
+	//Scores every player's score. Indexed by name
+	public static HashMap<String, Integer> scoreCache = null;
+	//Stores player names in the order of their position in the highscores
+	public static ArrayList<String> positions = null;
+	
 	public static void add(String playerName, Integer money) {
-		money = money + get(playerName);
-		Main.playerData.set("Players." + playerName + ".Points", money);
-		Main.playerData.save();
+		set(playerName, get(playerName) + money);
 	}
 
 	public static Boolean remove(String playerName, Integer money) {
 		money = get(playerName) - money;
 		if (money < 0)			return false;
-		Main.playerData.set("Players." + playerName + ".Points", money);
-		Main.playerData.save();
+		set(playerName, money);
 		return true;
 	}
 
 	public static Integer get(String playerName) {
-		if (Main.playerData.get("Players." + playerName + ".Points") == null)
+		if(scoreCache == null || positions == null) loadCache();
+		
+		if (scoreCache.get(playerName) == null)
 			return 0;
 		else
-			return Main.playerData.getInt("Players." + playerName + ".Points");
+			return scoreCache.get(playerName);
 	}
 
 	public static void set(String playerName, Integer money) {
+		if(scoreCache == null || positions == null) loadCache();
+		
+		if(scoreCache.get(playerName) == null){
+			scoreCache.put(playerName, money);
+			positions.add(playerName);
+		}
+		
 		Main.playerData.set("Players." + playerName + ".Points", money);
 		Main.playerData.save();
+		
+		//Maybe only resort every few minutes?
+		resortPositions();
 	}
 
 	public static void help(CommandSender s) {
@@ -62,8 +77,59 @@ public class Points {
 				+ " Shows you the highscore board");
 	}
 
+	public static void resortPositions()
+	{
+		if(positions == null || positions.size() != scoreCache.size()){
+			positions = new ArrayList<String>();
+			
+			for(Entry<String, Integer> entry : scoreCache.entrySet()){
+				positions.add(entry.getKey());
+			}
+		}
+		
+		//Bubble algorithm
+		int end = 0;
+		int edits;
+		String temp;
+		do{
+			end++;
+			edits = 0;
+			
+			for(int i = 0; i < positions.size()-end; i++)
+			{	
+				if(scoreCache.get(positions.get(i)) < scoreCache.get(positions.get(i+1)))
+				{
+					temp = positions.get(i);
+					positions.set(i, positions.get(i+1)); 
+					positions.set(i+1, temp);
+					edits++;
+				}
+			}
+			
+		}while(edits > 0);
+	}
+	public static void loadCache()
+	{
+		scoreCache = new HashMap<String, Integer>();
+		
+		ConfigurationSection cfgData = Main.playerData.getConfigurationSection("Players");
+		
+		//Load all player's scores
+		for(String player : cfgData.getKeys(false))
+		{
+			scoreCache.put(player, cfgData.getInt(player+".Points"));
+		}
+		
+		resortPositions();
+	}
+	
+	
 	public static ArrayList<String> highScore() {
-		ArrayList<String> players = new ArrayList<String>();
+		
+		if(scoreCache == null || positions == null) loadCache();
+		return (ArrayList<String>) positions.subList(0, 9);
+		
+		/*ArrayList<String> players = new ArrayList<String>();
 		HashMap<String, Integer> places = new HashMap<String, Integer>();
 		if (Main.playerData.getConfigurationSection("Players") == null)
 			return null;
@@ -79,7 +145,7 @@ public class Points {
 			for (Entry<String, Integer> entry : places.entrySet())
 				if (entry.getValue() == i && players.size() < 10)
 					players.add(entry.getKey());
-		return players;
+		return players;*/
 	}
 
 	public static void sendHighscore(final CommandSender s) {
